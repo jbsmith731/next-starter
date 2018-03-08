ec33b291ec94fad6bfa4c2f3003efa0bdf9ba9e7
Brett Smith <hello@thebrettsmith.com>
2018-03-08 16:28:51 -0600

    update layout & add css modules

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import "./Layout.scss"
+import s from "./Layout.scss"
 
 const Layout = (props) => (
   <div>
--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+module.exports = withSass({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: 'es_[hash:base64:6]',
+  }
+})
