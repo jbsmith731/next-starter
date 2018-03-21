11cce348feacbc82402a2fa41308d3e9c584680b
bsmith <brett@elegantseagulls.com>
2018-03-20 20:06:35 -0500

    update classNames for prod vs dev

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,12 @@
 const withSass = require('@zeit/next-sass');
 
+const dev = process.env.NODE_ENV !== 'production';
+
 module.exports = withSass({
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
-    localIdentName: 'es_[hash:base64:6]',
+    localIdentName: dev ? '[local]_[hash:base64:6]' : 'es_[hash:base64:6]',
   },
   useFileSystemPublicRoutes: false,
 });
