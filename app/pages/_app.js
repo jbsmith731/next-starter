199f7ef73b0e3bf49ddb720a6e7e76cea4f6415d
Brett Smith <hello@thebrettsmith.com>
2019-01-03 08:05:10 -0600

    add styled components, clean up packages, remove scss

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,11 +1,9 @@
 import App, { Container as NextContainer } from 'next/app';
 import React from 'react';
-import Layout from '../components/layout';
 
 export default class MyApp extends App {
 
-  // eslint-disable-next-line no-unused-vars
-  static async getInitialProps({ Component, router, ctx }) {
+  static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
 
     if (Component.getInitialProps) {
@@ -20,9 +18,7 @@ export default class MyApp extends App {
 
     return (
       <NextContainer>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Component {...pageProps} />
       </NextContainer>
     );
   }
--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,17 +1,51 @@
+/* eslint-disable jsx-a11y/html-has-lang */
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import Helmet from 'react-helmet';
+import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
+export default class extends Document {
+
+  static async getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, helmet: Helmet.renderStatic(), styleTags };
+  }
+
+  // should render on <html>
+  get helmetHtmlAttrComponents() {
+    return this.props.helmet.htmlAttributes.toComponent();
+  }
+
+  // should render on <body>
+  get helmetBodyAttrComponents() {
+    return this.props.helmet.bodyAttributes.toComponent();
+  }
+
+  // should render on <head>
+  get helmetHeadComponents() {
+    return Object.keys(this.props.helmet)
+      .filter(el => el !== 'htmlAttributes' && el !== 'bodyAttributes')
+      .map(el => this.props.helmet[el].toComponent());
+  }
+
+  get helmetJsx() {
+    return (
+      <Helmet />
+    );
+  }
 
   render() {
     return (
-      <html lang="en">
+      <html {...this.helmetHtmlAttrComponents}>
         <Head>
-          <title>Next.js Starter</title>
-          <link rel="stylesheet" href="/_next/static/style.css" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {this.helmetJsx}
+          {this.helmetHeadComponents}
+          {this.props.styleTags}
         </Head>
-        <body>
+        <body {...this.helmetBodyAttrComponents}>
           <Main />
           <NextScript />
         </body>
